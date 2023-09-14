d896dbbdec84c1306c9657b6b35f6d8a281a0ce0
practicante09 <cfeijoo@infosis.tech>
2023-09-14 11:49:35 -0300

    Add pepe credentials xd

--- app/src/main/java/com/mardelapps/oneapp/LoginVerification.kt
+++ app/src/main/java/com/mardelapps/oneapp/LoginVerification.kt
@@ -2,7 +2,9 @@ package com.mardelapps.oneapp
 
 class LoginVerification {
     fun verify(Email: String, Contrasena: String): Boolean {
-        return Email == "ciroFF@mardelapps.com" &&
-                Contrasena == "CC450"
+        return (Email == "ciroFF@mardelapps.com" &&
+                Contrasena == "CC450") ||
+                (Email == "pepe@gmail.com" &&
+                        Contrasena == "PP3")
     }
 }
